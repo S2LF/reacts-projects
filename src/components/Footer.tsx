6ccae78b1f5bb8f629af5d1e6b734756d859ca3b
s2lf <sdeuhelf@gmail.com>
2021-02-02 20:26:12 +0100

    Init project w Reactstrap & React router

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer(): JSX.Element {
+  return (
+    <footer className="bg-primary p-3 d-flex align-items-center justify-content-center text-center">
+      <div className="text-success">&copy;Syl20 @2021</div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Nav, Navbar, NavbarBrand, NavItem, NavLink } from 'reactstrap';
+import {
+  BrowserRouter as Router,
+  NavLink as RouterNavLink,
+  Route,
+  Switch,
+} from 'react-router-dom';
+import Home from './Home';
+import Test from './Test';
+
+function Header(): JSX.Element {
+  return (
+    <header>
+      <Router>
+        <Navbar color="primary">
+          <NavbarBrand
+            tag={RouterNavLink}
+            exact
+            to="/"
+            activeClassName="active"
+          >
+            Reactstrap
+          </NavbarBrand>
+          <Nav>
+            <NavItem>
+              <NavLink
+                tag={RouterNavLink}
+                exact
+                activeClassname="active"
+                to="/memory"
+              >
+                Memory
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink>Pendu</NavLink>
+            </NavItem>
+          </Nav>
+        </Navbar>
+
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/memory" exact component={Test} />
+        </Switch>
+      </Router>
+    </header>
+  );
+}
+
+export default Header;
